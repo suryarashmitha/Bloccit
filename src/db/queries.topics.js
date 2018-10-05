439da79e346dc9d80004669d3c5836d90e30731d
RD <rd@Suryas-MBP.attlocal.net>
2018-10-04 22:49:49 -0500

    checkpoint-resource-2 : completed

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -6,12 +6,25 @@ module.exports = {
   getAllTopics(callback){
     return Topic.all()
 
-//#2 When successful, the callback passed to the then method will execute. It contains all topics (if any) in the topics parameter. Inside, it calls the callback method passed into getAllTopics with null and the topics that came from the database. When we call getAllTopics from the controller, we'll pass a function that renders the view according to what we pass to the callback inside getAllTopics
+//#2
     .then((topics) => {
       callback(null, topics);
     })
     .catch((err) => {
       callback(err);
     })
-  }
+  },
+
+  addTopic(newTopic, callback){
+      return Topic.create({
+        title: newTopic.title,
+        description: newTopic.description
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+    }
 }
